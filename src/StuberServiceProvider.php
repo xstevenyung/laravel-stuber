d93cd5ea00d4b4cf92988019db346dad9fcc594e
Steven Yung <stevenyung@icloud.com>
2018-03-18 21:14:11 +0100

    add StuberServiceProvider

---
+++ src/StuberServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace StvnYung\LaravelStuber;
+
+use Illuminate\Support\ServiceProvider;
+
+class StuberServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        //
+    }
+
+    public function register()
+    {
+        //
+    }
+}
