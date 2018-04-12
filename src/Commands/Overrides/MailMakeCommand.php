c978a31020206f70afdebc6da2d42a55a9e51463
Steven Yung <stevenyung@icloud.com>
2018-04-12 17:57:25 +0200

    clean up MailMakeCommand

--- src/Commands/Overrides/MailMakeCommand.php
+++ src/Commands/Overrides/MailMakeCommand.php
@@ -8,15 +8,4 @@ use Illuminate\Foundation\Console\MailMakeCommand as BaseMailMakeCommand;
 class MailMakeCommand extends BaseMailMakeCommand
 {
     use OverridableMakeCommand;
-
-    protected function writeMarkdownTemplate()
-    {
-        $path = resource_path('views/' . str_replace('.', '/', $this->option('markdown'))) . '.blade.php';
-
-        if (!$this->files->isDirectory(dirname($path))) {
-            $this->files->makeDirectory(dirname($path), 0755, true);
-        }
-
-        $this->files->put($path, file_get_contents(__DIR__ . '/stubs/markdown.stub'));
-    }
 }
